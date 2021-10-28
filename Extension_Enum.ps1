3604867f6d1ef1b1c331d4f02ba1de641ffae418
Alee <34715755+h20lee@users.noreply.github.com>
2021-10-28 15:38:40 +0800

    Add files via upload

---
+++ Extension_Enum.ps1
@@ -0,0 +1,26 @@
+﻿$hash = @{}
+$hash2 = @{}
+$fileCount = 0
+$folderCount = 0
+$files = Get-ChildItem -Path 'C:\temp' -Recurse
+foreach ($file in $files) {
+    if (!$file.PSIsContainer) {
+        if ($hash.ContainsKey($file.Extension)) {
+            $hash[$file.Extension] = $hash[$file.Extension] + 1
+            $hash2[$file.Extension] = $hash2[$file.Extension] + ($file.length / 1MB)
+        } else {
+            $hash[$file.Extension] = 1
+            $hash2[$file.Extension] = ($file.length / 1MB)
+        }
+        $fileCount += 1    
+    } else {
+        $folderCount += 1
+    }
+}
+
+$hash.keys | ForEach-Object {
+    Write-Output "$($_),$($hash[$_]),$([math]::Round(($hash2[$_])))"
+    }
+
+Write-Host "========================= Summary ========================="
+Write-Host "Files $($fileCount), Folders $($folderCount)"
